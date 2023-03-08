9cea0f269185f93867f5056a588794558981b8aa
ARATA Mizuki <minorinoki@gmail.com>
2023-03-08 21:29:17 +0900

    purescript 0.15.8

--- example/build.sh
+++ example/build.sh
@@ -1,3 +1,3 @@
 npx spago build
-stack exec --stack-yaml=../stack-purs0.15.7.yaml -- purs-tsd-gen -d output
+stack exec --stack-yaml=../stack-purs0.15.8.yaml -- purs-tsd-gen -d output
 npx tsc --strict --module es2015 user.mts
